9b40d19fe1f2d9125ce759c152a7a3ea16d4d516
cassiopeiarepo <cassiopeia@cassiopeia.cal24.pl>
2022-08-27 14:24:34 +0200

    add node

---
+++ node/NodeTypeList.cpp
@@ -0,0 +1,25 @@
+#include "Core/Types/NodeTypeList.h"
+
+Node* NodeTypeList::createNode(const char* nodeTypeName, Node* parent) {
+
+	NodeType* nodeType = getType(nodeTypeName);
+
+	if (!nodeType)
+		return NULL;
+
+	Node* node = NULL;
+
+	if (IsNative()) {
+		
+	}
+	else {
+		node = new Node();
+		node->getPropertySet()->createFields();
+	}
+
+	if (parent != NULL)
+		node->setParent(parent);
+
+
+	return node;
+}
